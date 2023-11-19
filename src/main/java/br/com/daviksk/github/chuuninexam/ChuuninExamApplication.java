0934df754a460f2c7ca8f6d3368242b8f9118977
davi <davikraieski@gmail.com>
2023-11-19 18:03:36 -0300

    initial commit

---
+++ src/main/java/br/com/daviksk/github/chuuninexam/ChuuninExamApplication.java
@@ -0,0 +1,13 @@
+package br.com.daviksk.github.chuuninexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuuninExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuuninExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/daviksk/github/chuuninexam/ChuuninExamApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daviksk.github.chuuninexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuuninExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
